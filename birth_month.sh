7452bd75e0777f0607e6ce26fa905e6aa177b117
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-24 19:57:37 +0530

    added:day8

---
+++ birth_month.sh
@@ -0,0 +1,14 @@
+#program to display birth-month of 50 indiviuals with date
+for((i=0;i<=50;i++))
+do
+	month[$i]=$((RANDOM%12 +1))
+done
+
+for((i=0;i<=50;i++))
+do
+	day[$i]=$((RANDOM%30 +1))
+done
+for((i=0;i<=50;i++))
+do
+		echo "person[$i] - month ${month[i]} - day ${day[i]}"
+done
