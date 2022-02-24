aecf48e8f8c780caca9714a8d2df5f6203f9b4a5
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-24 19:04:34 +0530

    added:dice

---
+++ dice.sh
@@ -0,0 +1,12 @@
+# program of dice with dictionary
+dice=0
+while (( result[$dice] < 10 ))
+do
+    dice=$((RANDOM%6+1))
+    result[$dice]=$(( ${result[$dice]} + 1 ))
+done
+
+echo "Dice number: " ${!result[@]}
+echo "no.of times corresponding number repeated: " ${result[@]}
+echo "Max Repeated Dice Number: " $dice
+
